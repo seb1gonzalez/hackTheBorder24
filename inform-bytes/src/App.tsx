520a46ac774e700adcc29349739e9f3873a0c468
Sebastian Gonzalez <sebastian95gonzalez@gmail.com>
2024-11-22 15:08:51 -0700

    UI search and results UI

--- inform-bytes/src/App.tsx
+++ inform-bytes/src/App.tsx
@@ -2,10 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import useNewsApi from './hooks/useNewsApi';
 import { INewsApiEverythingParams } from 'ts-newsapi';
+import Search from './components/SearchBar/SearchBar';
+import { Divider, Grid2, Stack, Typography } from '@mui/material';
+import { useMemo, useState } from 'react';
+import NewsCard from './components/NewsCard';
 
 function App() {
+  const [question, setQuestion] = useState('');
   const newsQuery: INewsApiEverythingParams = {
-    q: 'kamala harris wins',
+    q: question,
     from: '2024-11-01',
     sortBy: 'relevancy',
     language: 'en',
@@ -14,6 +19,19 @@ function App() {
   };
   const results = useNewsApi(newsQuery);
 
+  const filteredData = useMemo(() => {
+    return results?.articles.filter((a) => !!a.author).map((article) => (
+      <NewsCard
+        title={article.title}
+        content={article.description ?? ''}
+        image={article.urlToImage ?? ''}
+        href={article.url}
+
+      />
+    )) ?? []
+  }, [results])
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,13 +39,25 @@ function App() {
         <p>
           NEWS API
         </p>
-        {results?.articles.filter((a) => !!a.author).map((article) => (
-          <ul>
-            <li key={article.url}>
-              {article.title}
-            </li>
-          </ul>
-        ))}
+        <Search 
+          setQuestion={setQuestion}
+        />
+        <Typography style={{padding: 5, color: 'black'}}>
+        Your question was: {question}
+        </Typography>
+        <Grid2 container spacing={2}>
+        <Grid2 size={6}>
+            AI SEARCH
+            <Divider />
+        </Grid2>
+          <Grid2 size={6}>
+            SOURCES
+            <Divider />
+            <Stack useFlexGap spacing={5} mt={'20px'}>
+              {filteredData}
+            </Stack>
+          </Grid2>
+        </Grid2>
       </header>
     </div>
   );
